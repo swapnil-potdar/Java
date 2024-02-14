9196acfbf1b14422872450ca1d75d0137c60a23a
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-02-14 22:37:09 +0530

    string comparison using equals()

---
+++ StringCompareequl.java
@@ -0,0 +1,9 @@
+public class StringCompareequl{
+   public static void main(String []args){
+      String s1 = "SwapnilPotdar";
+      String s2 = "SwapnilPotdar";
+      String s3 = new String ("Swapnil Potdar");
+      System.out.println(s1.equals(s2));
+      System.out.println(s2.equals(s3));
+   }
+}
