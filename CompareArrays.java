397275c407285e193d5b49bcbc987a774171d63d
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-02-22 21:53:57 +0530

    compare two arrays

---
+++ CompareArrays.java
@@ -0,0 +1,9 @@
+public class CompareArrays { 
+   public static void main (String[] args) {
+      int arr1[] = {1, 2, 3};
+      int arr2[] = {1, 2, 3};
+      
+      if (arr1 == arr2) System.out.println("Same"); 
+      else System.out.println("Not same");
+   } 
+}
