7284bd919fe4f1a8a3ad37efbd2107c12a49ec9c
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-03-28 22:58:04 +0530

    method overloading

---
+++ overloading.java
@@ -0,0 +1,10 @@
+public class Calculation {
+   void sum(int a,int b){System.out.println(a+b);}
+   void sum(int a,int b,int c){System.out.println(a+b+c);}
+
+   public static void main(String args[]){
+      Calculation cal = new Calculation();
+      cal.sum(20,30,60);
+      cal.sum(20,20);
+   }
+}
